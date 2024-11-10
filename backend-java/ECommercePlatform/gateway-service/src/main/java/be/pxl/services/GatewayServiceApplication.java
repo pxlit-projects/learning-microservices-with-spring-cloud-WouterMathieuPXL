efd5872c7b9dcb47f9f6b7e3d231f95130926477
WouterMathieuPXL <Wouter.Mathieu@student.pxl.be>
2024-11-10 14:14:37 +0100

    gateway-service

--- backend-java/ECommercePlatform/gateway-service/src/main/java/be/pxl/services/GatewayServiceApplication.java
+++ backend-java/ECommercePlatform/gateway-service/src/main/java/be/pxl/services/GatewayServiceApplication.java
@@ -3,14 +3,12 @@ package be.pxl.services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class ShoppingCartApplication {
+public class GatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShoppingCartApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 }
--- backend-java/ECommercePlatform/logbook-service/src/main/java/be/pxl/services/LogBookServiceApplication.java
+++ backend-java/ECommercePlatform/logbook-service/src/main/java/be/pxl/services/LogBookServiceApplication.java
@@ -6,11 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class LogBookServiceApplication
-{
+public class LogBookServiceApplication {
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         SpringApplication.run(LogBookServiceApplication.class, args);
     }
 }
