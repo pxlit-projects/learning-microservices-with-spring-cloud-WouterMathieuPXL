55e581ef6c32a574f5c20de1d2ce0a8d2fa3304a
WouterMathieuPXL <Wouter.Mathieu@student.pxl.be>
2024-11-10 22:15:00 +0100

    Logbook-service logback

--- backend-java/ECommercePlatform/logbook-service/src/main/java/be/pxl/services/services/QueueService.java
+++ backend-java/ECommercePlatform/logbook-service/src/main/java/be/pxl/services/services/QueueService.java
@@ -2,6 +2,8 @@ package be.pxl.services.services;
 
 import be.pxl.services.audit.AuditLogRequest;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +11,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class QueueService {
 
+    private static final Logger logger = LoggerFactory.getLogger(QueueService.class);
     private final AuditLogService auditLogService;
 
     @RabbitListener(queues = "auditQueue")
     public void listen(AuditLogRequest auditLog) {
-        auditLogService.log(auditLog.getProductId(), auditLog.getAction(), auditLog.getPerformedBy(), auditLog.getTimestamp());
+        logger.info("Received message from auditQueue: {}", auditLog);
+        auditLogService.log(
+                auditLog.getProductId(),
+                auditLog.getAction(),
+                auditLog.getPerformedBy(),
+                auditLog.getTimestamp());
+        logger.info("Audit log processed successfully");
     }
 }
